9a81cb942033769587560a93e26586e6ca26454e
Vlad Kalinichenko <vladkalinichenko@MacBook-Pro-Vlad.local>
2020-01-24 15:01:04 +0200

    all

---
+++ src/components/Main/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Main.module.css';
+
+const Main = (props) => {
+  return (
+    <div className={s.test}>
+      TEST
+    </div>
+  )
+}
+
+export default Main;
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import App from "./components/Main/Main";
+ReactDOM.render(<App />, document.querySelector("#root"));
